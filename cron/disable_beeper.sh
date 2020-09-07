f382ec674cf6c0e03986ea9a4ebd754817dbe304
Bryan Horstmann <bhorstmann@gmail.com>
2020-09-07 18:31:17 +0200

    Create a cron to disable ups beeper

---
+++ cron/disable_beeper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+UPS_NAME=ups
+USERNAME=admin
+PASSWORD=admin
+
+# Get the current status
+STATUS=$(upsc ${UPS_NAME} ups.beeper.status 2>/dev/null)
+
+# If current status is not disabled, then disable it
+if [[ "${STATUS}" != "disabled" ]]; then
+  upscmd -u ${USERNAME} -p ${PASSWORD} ${UPS_NAME} beeper.disable
+fi
+
