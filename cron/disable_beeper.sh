9997099875d8abe6f0ce1646b5d83e1b02920da3
Bryan Horstmann <36621621+bryanhorstmann@users.noreply.github.com>
2021-08-17 11:27:36 +0200

    Mecer 2000 (#1)
    * New Mecer 2000
    
    * Update README

--- cron/disable_beeper.sh
+++ cron/disable_beeper.sh
@@ -9,6 +9,6 @@ STATUS=$(upsc ${UPS_NAME} ups.beeper.status 2>/dev/null)
 
 # If current status is not disabled, then disable it
 if [[ "${STATUS}" != "disabled" ]]; then
-  upscmd -u ${USERNAME} -p ${PASSWORD} ${UPS_NAME} beeper.disable
+  upscmd -u ${USERNAME} -p ${PASSWORD} ${UPS_NAME} beeper.toggle
 fi
 
